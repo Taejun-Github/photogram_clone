0327ff2d635979e6c80ac4a1b39c100af90ef8da
Taejun-Github <tjkimin2021@gmail.com>
2022-01-03 13:46:14 +0900

    공통응답 Script 만들기

--- src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
+++ src/main/java/com/cos/photogramstart/handler/ControllerExceptionHandler.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cos.photogramstart.handler.ex.CustomValidationException;
-import com.cos.photogramstart.web.dto.CMRespDto;
+import com.cos.photogramstart.util.Script;
 
 @ControllerAdvice
 @RestController
@@ -15,11 +15,22 @@ public class ControllerExceptionHandler {
 
 	@ExceptionHandler(CustomValidationException.class)
 	//런타임 Exception이 발동하는 모든 것에 대해서 
-	public CMRespDto<Map<String, String>> validationException(CustomValidationException e) {
-		//여기서는 errorMap을 담아야 하므로 CMRespDto의 제네릭을 map으로 설정해 준다.
-			return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
+	public String validationException(CustomValidationException e) {
+		
+			return Script.back(e.getMessage().toString());
+		
+			//공통응답 dto로 응답하는 방법은 이렇다.
+			//return new CMRespDto<Map<String, String>>(-1, e.getMessage(), e.getErrorMap());
 			//만약 CMRespDto의 세번째 인자에다가 String을 넣고 싶다면?
 			//함수의 반환타입을 CMResDto<String> 타입으로 설정해주면 된다.
 			//그리고 아무거나 넣고 싶다면 CMRespDto<?>를 써주면 된다.
+			
+			
+			
+			//정리
+			//CMRespDto, Script를 비교하면?
+			//1. 클라이언트에게 응답할 때는 Script가 좋다.
+			//2. Ajax 통신 - CMRespDto가 좋다
+			//3. Android 통신 - CMRespDto가 좋다.
 	}
 }
