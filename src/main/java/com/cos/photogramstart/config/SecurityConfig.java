1317198308d9522c328bbfb9699caaa4a10d9973
TAEJUN\ausch <tjkimin2021@gmail.com>
2022-02-22 17:49:49 +0900

    oauth 구현 완료

--- src/main/java/com/cos/photogramstart/config/SecurityConfig.java
+++ src/main/java/com/cos/photogramstart/config/SecurityConfig.java
@@ -7,9 +7,16 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
+import com.cos.photogramstart.config.oauth.OAuth2DetailsService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @EnableWebSecurity //해당 파일로 시큐리티를 활성화한다는 뜻의 어노테이션이다.
 @Configuration // IoC 메모리에 등록한다.
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+	
+	private final OAuth2DetailsService oauth2DetailsService;
 	
 	//IoC 컨테이너가 들고있게 된다.
 	@Bean
@@ -30,7 +37,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
 			.formLogin()
 			.loginPage("/auth/signin") //GET으로 접근했을 때
 			.loginProcessingUrl("/auth/signin") //POST로 접근했을 때 스프링 시큐리티가 로그인 프로세스를 진행한다.
-			.defaultSuccessUrl("/");
+			.defaultSuccessUrl("/")
+			.and()
+			.oauth2Login() //form로그인도 하는데, oauth2 로그인도 한다는 뜻이다.
+			.userInfoEndpoint() // oauth2 로그인을 하면 최종 응답을 회원정보를 바로 받을 수 있다.
+			.userService(oauth2DetailsService);
 		//어떤 뜻? antMatchers에서 지정한 링크는 로그인을 요구하고 나머지 링크는 허용한다.
 		//그리고 로그인으로 할 페이지는 /auth/signin 페이지로 간다.
 		//그리고 로그인이 정상적으로 되면 / 페이지로 이동한다.
